125de9126b32af55bc01ef8b69a27caae814acc9
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-11-17 22:02:33 +0100

    added depth testing

--- main.cc
+++ main.cc
@@ -9,134 +9,10 @@ namespace rl {
 }
 
 #include "math.h"
+#include "ras.h"
 #include "aux.h"
 
-struct Color {
-    uint8_t r = 0;
-    uint8_t g = 0;
-    uint8_t b = 0;
-    uint8_t a = 0xff;
-
-    constexpr Color operator*(float value) const {
-        return {
-            static_cast<uint8_t>(r * value),
-            static_cast<uint8_t>(g * value),
-            static_cast<uint8_t>(b * value),
-            static_cast<uint8_t>(a * value),
-        };
-    }
-
-    constexpr Color operator+(Color other) const {
-        return {
-            static_cast<uint8_t>(r + other.r),
-            static_cast<uint8_t>(g + other.g),
-            static_cast<uint8_t>(b + other.b),
-            static_cast<uint8_t>(a + other.a),
-        };
-    }
-
-};
-
-static_assert(sizeof(Color) == 4);
-
-template <typename T>
-class Buffer {
-    const int m_width;
-    const int m_height;
-    std::vector<T> m_buffer;
-
-public:
-    Buffer(int width, int height)
-        : m_width(width)
-        , m_height(height)
-        , m_buffer(m_width * m_height)
-    { }
-
-    void write(int x, int y, T value) {
-        m_buffer[y * m_width + x] = value;
-    }
-
-    [[nodiscard]] T get(int x, int y) const {
-        return m_buffer[y * m_width + x];
-    }
-
-    void clear(T value) {
-        for (int x = 0; x < get_width(); ++x) {
-            for (int y = 0; y < get_height(); ++y) {
-                write(x, y, value);
-            }
-        }
-    }
-
-    [[nodiscard]] int get_width() const {
-        return m_width;
-    }
-
-    [[nodiscard]] int get_height() const {
-        return m_height;
-    }
-
-};
-
-using ColorBuffer = Buffer<Color>;
-using DepthBuffer = Buffer<float>;
-
-class Rasterizer {
-    ColorBuffer& m_color_buffer;
-    DepthBuffer& m_depth_buffer;
-
-public:
-    Rasterizer(ColorBuffer& color_buffer, DepthBuffer& depth_buffer)
-        : m_color_buffer(color_buffer)
-        , m_depth_buffer(depth_buffer)
-    {
-        m_color_buffer.clear({0x0, 0x0, 0x0, 0xff});
-        m_depth_buffer.clear(0.0f);
-    }
-
-    void draw_triangle(Vec a, Vec b, Vec c, Color color) {
-
-        // TODO: only check bounding box of triangle
-        for (float x = 0; x < m_color_buffer.get_width(); ++x) {
-            for (float y = 0; y < m_color_buffer.get_height(); ++y) {
-                Vec p { x, y, 0.0f, 1.0f };
-
-                int abc = edge_function(a, b, c);
-                int abp = edge_function(a, b, p);
-                int bcp = edge_function(b, c, p);
-                int cap = edge_function(c, a, p);
-
-                float factor_a = static_cast<float>(bcp) / abc;
-                float factor_b = static_cast<float>(cap) / abc;
-                float factor_c = static_cast<float>(abp) / abc;
-
-                Color c = Color(0xff, 0x0, 0x0, 0xff) * factor_a +
-                          Color(0x0, 0xff, 0x0, 0xff) * factor_b +
-                          Color(0x0, 0x0, 0xff, 0xff) * factor_c;
-
-                // float depth = a.z;
-                // float stored_depth = m_depth_buffer.get(x, y);
-                //
-                // if (stored_depth > depth) continue;
-
-                if (abp > 0 && bcp > 0 && cap > 0) {
-                    // m_depth_buffer.write(x, y, depth);
-                    m_color_buffer.write(x, y, c);
-                }
-
-            }
-        }
-
-    }
-
-private:
-    // returns the area of a triangle, which may be negative
-    [[nodiscard]] static constexpr float edge_function(Vec a, Vec b, Vec c) {
-        return (b.x-a.x)*(c.y-a.y) - (b.y-a.y)*(c.x-a.x);
-    }
-
-};
-
+// TODO: wavefront obj loader
 
 int main() {
 
@@ -157,42 +33,49 @@ int main() {
 
     Cube cube_copy(cube);
 
-    ras.draw_triangle(
-        {0, 0, 0, 0},
-        {static_cast<float>(width), 0, 0, 0},
-        {width/2.0f, static_cast<float>(height), 0, 0},
-        Color(0xff, 0x0, 0x0, 0xff)
-    );
+    float h = height;
+    float w = width;
+    float z = -1.0;
+
+    // ras.draw_triangle(
+    //     {w/2, 0, 0, 0},
+    //     {w, h, 0, 0},
+    //     {0, h, 0, 0},
+    //     Color::blue()
+    // );
+    //
+    // ras.draw_triangle(
+    //     {0, 0, z, 0},
+    //     {w, 0, z, 0},
+    //     {w/2, h, z, 0},
+    //     Color::red()
+    // );
 
     for (auto& face : cube_copy.faces) {
         for (auto& t : face.triangles) {
-            auto rot_mat = Mat::rotate(Vec {1.0f, 1.0f, 0.0f, 1.0f}, deg_to_rad(15));
+            auto rot_mat = Mat::rotate(Vec {1.0f, 1.0f, 0.0f, 1.0f}, deg_to_rad(45));
             t.a = rot_mat * t.a;
             t.b = rot_mat * t.b;
             t.c = rot_mat * t.c;
         }
     }
 
-    // for (auto&& [face_nr, face] : cube_copy.faces | std::views::enumerate) {
-    //     for (auto& t : face.triangles) {
-    //         auto color = face_color_map[face_nr];
-    //         float w = 200;
-    //         float h = 200;
-    //         float d = 200;
-    //         Vec offset {width/2.0f-w/2.0f, height/2.0f-h/2.0f};
-    //         ras.draw_triangle(t.a*w+offset, t.b*h+offset, t.c*d+offset, color);
-    //     }
-    // }
-
+    for (auto&& [face_nr, face] : cube_copy.faces | std::views::enumerate) {
+        for (auto& t : face.triangles) {
+            auto color = face_color_map[face_nr];
+            float w = 100;
+            float h = 100;
+            float d = 100;
+            Vec offset {width/2.0f-w/2.0f, height/2.0f-h/2.0f};
+            ras.draw_triangle(t.a*w+offset, t.b*h+offset, t.c*d+offset, color);
+        }
+    }
 
     rl::InitWindow(width, height, "ras");
 
     while (!rl::WindowShouldClose()) {
         rl::BeginDrawing();
         rl::ClearBackground(rl::BLACK);
-
-
-
 
         for (int x = 0; x < width; ++x) {
             for (int y = 0; y < height; ++y) {
---
+++ ras.h
@@ -0,0 +1,152 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+#include "math.h"
+
+struct Color {
+    uint8_t r = 0;
+    uint8_t g = 0;
+    uint8_t b = 0;
+    uint8_t a = 0xff;
+
+    [[nodiscard]] static constexpr Color blue() {
+        return {0x0, 0x0, 0xff, 0xff};
+    }
+
+    [[nodiscard]] static constexpr Color red() {
+        return {0xff, 0x0, 0x0, 0xff};
+    }
+
+    [[nodiscard]] static constexpr Color green() {
+        return {0x0, 0xff, 0x0, 0xff};
+    }
+
+    constexpr Color operator*(float value) const {
+        return {
+            static_cast<uint8_t>(r * value),
+            static_cast<uint8_t>(g * value),
+            static_cast<uint8_t>(b * value),
+            static_cast<uint8_t>(a * value),
+        };
+    }
+
+    constexpr Color operator+(Color other) const {
+        return {
+            static_cast<uint8_t>(r + other.r),
+            static_cast<uint8_t>(g + other.g),
+            static_cast<uint8_t>(b + other.b),
+            static_cast<uint8_t>(a + other.a),
+        };
+    }
+
+};
+
+static_assert(sizeof(Color) == 4);
+
+template <typename T>
+class Buffer {
+    const int m_width;
+    const int m_height;
+    std::vector<T> m_buffer;
+
+public:
+    Buffer(int width, int height)
+        : m_width(width)
+        , m_height(height)
+        , m_buffer(m_width * m_height)
+    { }
+
+    void write(int x, int y, T value) {
+        m_buffer[y * m_width + x] = value;
+    }
+
+    [[nodiscard]] T get(int x, int y) const {
+        return m_buffer[y * m_width + x];
+    }
+
+    void clear(T value) {
+        for (int x = 0; x < get_width(); ++x) {
+            for (int y = 0; y < get_height(); ++y) {
+                write(x, y, value);
+            }
+        }
+    }
+
+    [[nodiscard]] int get_width() const {
+        return m_width;
+    }
+
+    [[nodiscard]] int get_height() const {
+        return m_height;
+    }
+
+};
+
+using ColorBuffer = Buffer<Color>;
+using DepthBuffer = Buffer<float>;
+
+class Rasterizer {
+    ColorBuffer& m_color_buffer;
+    DepthBuffer& m_depth_buffer;
+
+public:
+    Rasterizer(ColorBuffer& color_buffer, DepthBuffer& depth_buffer)
+        : m_color_buffer(color_buffer)
+        , m_depth_buffer(depth_buffer)
+    {
+        m_color_buffer.clear({0x0, 0x0, 0x0, 0xff});
+        m_depth_buffer.clear(-std::numeric_limits<float>::max());
+    }
+
+    void draw_triangle(Vec a, Vec b, Vec c, Color color) {
+
+        // TODO: only check bounding box of triangle
+        for (float x = 0; x < m_color_buffer.get_width(); ++x) {
+            for (float y = 0; y < m_color_buffer.get_height(); ++y) {
+                Vec p { x, y, 0.0f, 1.0f };
+
+                int abc = edge_function(a, b, c);
+                int abp = edge_function(a, b, p);
+                int bcp = edge_function(b, c, p);
+                int cap = edge_function(c, a, p);
+
+                float weight_a = static_cast<float>(bcp) / abc;
+                float weight_b = static_cast<float>(cap) / abc;
+                float weight_c = static_cast<float>(abp) / abc;
+
+                float depth = a.z * weight_a +
+                              b.z * weight_b +
+                              c.z * weight_c;
+
+                Color col = Color::red()   * weight_a +
+                            Color::green() * weight_b +
+                            Color::blue()  * weight_c;
+
+                // color = Color(depth, 1-depth, 1-depth, 0xff);
+
+                float stored_depth = m_depth_buffer.get(x, y);
+
+                // TODO: wireframe mode
+                // TODO: blending
+
+                if (depth < stored_depth) continue;
+
+                if (abp >= 0 && bcp >= 0 && cap >= 0) {
+                    m_color_buffer.write(x, y, color);
+                    m_depth_buffer.write(x, y, depth);
+                }
+
+            }
+        }
+
+    }
+
+private:
+    // returns the area of a triangle, which may be negative
+    [[nodiscard]] static constexpr float edge_function(Vec a, Vec b, Vec c) {
+        return (b.x-a.x)*(c.y-a.y) - (b.y-a.y)*(c.x-a.x);
+    }
+
+};
